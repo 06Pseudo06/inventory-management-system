305b46feaa964675f97b0bfcf5e83028f87a4473
06Pseudo06 <186454190+06Pseudo06@users.noreply.github.com>
2026-01-27 21:29:37 +0530

    Add centered pagination to item list

--- inventory/views/item_views.py
+++ inventory/views/item_views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from inventory.models import Item
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+
+from inventory.models import Item
+
 
 @login_required
 def item_list(request):
-    items = Item.objects.filter(is_active=True).order_by('name')
-    return render(request, 'inventory/item_list.html', {'items': items})
+    items_qs = Item.objects.filter(is_active=True).order_by("name")
+
+    paginator = Paginator(items_qs, 10)  
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        "items": page_obj,        
+        "page_obj": page_obj,
+    }
+
+    return render(request, "inventory/item_list.html", context)
